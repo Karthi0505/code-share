f777779e9a6a7f26802263df0ceb9abeb3a99ea6
karthi0505 <karthi.uideveloper@gmail.com>
2024-02-21 09:38:26 +0530

    commitMessage

---
+++ Objects/array_literal.js
@@ -0,0 +1,34 @@
+const arr1 = [1,2,3,true,false,undefined,[1,4,6],null,55,40, {a:10, b:20}, 60];
+
+console.log(
+    arr1[10].a
+)
+
+
+const obj1 = [
+    {
+        studentName: "Karthi",
+        std: "X", 
+        pass: true,
+        height: "165cm",
+        weight: "65kg"
+    },
+    {
+        studentName: "Daniel",
+        std: "X", 
+        pass: true,
+        height: "169cm",
+        weight: "55kg"
+    },
+    {
+        studentName: "Navas",
+        std: "XI", 
+        pass: false,
+        height: "185cm",
+        weight: "60kg"
+    }
+];
+
+//Array methods
+
+
